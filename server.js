cf4edf4216f2beadbd1974756bb42beb1b4ce7d4
Weilyu Wang <weilyuwang@gmail.com>
2019-03-17 22:10:50 -0400

    updating database4

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "process.env.DATABASE_URL", // heroku postgreSQL
+    connectionString: "process.env.DATABASE_URL", // heroku postgreSQL
     ssl: true
   }
 });
